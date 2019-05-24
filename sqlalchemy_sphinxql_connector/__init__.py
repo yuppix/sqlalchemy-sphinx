61f7625bd15ebc488d60d06bb0acf05bc2245488
Yuppix <git@yuppix.com>
2019-05-24 19:10:16 +0200

    set default connector to mysqldb

--- sqlalchemy_sphinxql_connector/__init__.py
+++ sqlalchemy_sphinxql_connector/__init__.py
@@ -3,6 +3,6 @@ from sqlalchemy.dialects import registry
 __version__ = "0.1.1"
 
 # https://bitbucket.org/zzzeek/sqlalchemy/issues/3536/engine-entrypoint-plugins
-registry.register("sphinx", "sqlalchemy_sphinxql_connector.mysql_connector", "Dialect")
+registry.register("sphinx", "sqlalchemy_sphinxql_connector.mysqldb", "Dialect")
 registry.register("sphinx.cymsql", "sqlalchemy_sphinxql_connector.cymysql", "Dialect")
 registry.register("sphinx.mysqldb", "sqlalchemy_sphinxql_connector.mysqldb", "Dialect")
